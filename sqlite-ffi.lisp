3f09c5f73e52ba7bdb20df0417dccffdbe56226d
OrganicProgramming <98908762+OrganicProgramming@users.noreply.github.com>
2023-08-05 22:30:52 +0200

    Added name for native windows dll (Windows 10)
    Normally the winsqlite3 dll file will be present by default on Windows 10 (and possibly 11), so this change allows this library to compile without any third party installation of sqlite3.

--- sqlite-ffi.lisp
+++ sqlite-ffi.lisp
@@ -37,7 +37,7 @@
 (define-foreign-library sqlite3-lib
   (:darwin (:default "libsqlite3"))
   (:unix (:or "libsqlite3.so.0" "libsqlite3.so"))
-  (t (:or (:default "libsqlite3") (:default "sqlite3"))))
+  (t (:or (:default "libsqlite3") (:default "sqlite3") (:default "winsqlite3"))))
 
 (use-foreign-library sqlite3-lib)
 
